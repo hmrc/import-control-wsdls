7cd7d3db33c5853001cee387cddef25db1c288b0
ChrisHillCap <26716179+ChrisHillCap@users.noreply.github.com>
2022-01-07 10:45:17 +0000

    flexmark

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.18.0" % Test,
-    "com.vladsch.flexmark"   % "flexmark-all"             % "0.36.8" % "test, it",
+    "com.vladsch.flexmark"   % "flexmark-all"             % "0.62.2" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"  % "test, it",
     "com.github.tomakehurst" % "wiremock-jre8-standalone" % "2.32.0" % "test, it"
   )

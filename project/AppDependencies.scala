96d6c7544a43c17dae314bc1b054916f60198c1d
Veera Kota <veeraskota@gmail.com>
2022-04-12 12:56:05 +0100

    ICS2-2763 upgraded wiremock dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,6 +11,6 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.22.0" % Test,
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.36.8" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"  % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8-standalone" % "2.32.0" % "test, it"
+    "com.github.tomakehurst" % "wiremock-jre8-standalone" % "2.33.1" % "test, it"
   )
 }

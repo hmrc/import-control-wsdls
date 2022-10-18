b9529b9b19a8348eb661e34f5e17bde1ccc27dcd
ansahdavid <45106722+ansahdavid@users.noreply.github.com>
2022-10-18 13:09:34 +0100

    ICS2-2931: Upgraded Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"      %% "bootstrap-backend-play-28" % "5.24.0",
-    "org.apache.axis2" % "axis2-kernel"               % "1.8.1"
+    "uk.gov.hmrc"      %% "bootstrap-backend-play-28" % "7.8.0",
+    "org.apache.axis2" % "axis2-kernel"               % "1.8.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.24.0" % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "7.8.0" % Test,
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.36.8" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"  % "test, it",
     "com.github.tomakehurst" % "wiremock-jre8-standalone" % "2.33.2" % "test, it"

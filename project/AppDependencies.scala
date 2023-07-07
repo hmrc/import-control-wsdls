c0add9c4331632d8eb9723cd86217193d60759c6
Veera Kota <veeraskota@gmail.com>
2023-07-07 15:01:42 +0100

    Removed config suggested by platops-pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,10 +11,9 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % bootstrapVersion % Test,
-    "org.scalatest"            %% "scalatest"                 % "3.2.16"         % "test, it",
-    "com.vladsch.flexmark"      % "flexmark-profile-pegdown"  % "0.64.8"         % "test, it",
+    "org.scalatest"          %% "scalatest"                   % "3.2.16"         % "test, it",
+    "com.vladsch.flexmark"   % "flexmark-profile-pegdown"     % "0.64.8"         % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"          % "5.1.0"          % "test, it",
     "com.github.tomakehurst" % "wiremock-jre8-standalone"     % "2.35.0"         % "test, it"
   )
 }
-

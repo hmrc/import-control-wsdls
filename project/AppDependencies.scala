ee7dda8be97f8b2dac88ff914f6af8f0758c4a60
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2024-01-23 10:08:12 +0000

    DCWL-1914 DCWL-1914 Replace axis2-kernel dependency by referring to wsdl4j directly (#47)
    * DCWL-1914 Replace axis2-kernel dependency by referring to wsdl4j directly

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "8.2.0"
+  lazy val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
     "uk.gov.hmrc"        %% "bootstrap-backend-play-30"   % bootstrapVersion,
-    "org.apache.axis2" % "axis2-kernel"               % "1.8.2"
+    "wsdl4j"              % "wsdl4j"                      % "1.6.3"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"      % bootstrapVersion % Test,
-    "com.vladsch.flexmark"   % "flexmark-profile-pegdown"     % "0.64.8"         % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.0"          % "test, it",
-    "org.wiremock"            % "wiremock-standalone"         % "3.3.1"           % "test, it"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"      % bootstrapVersion % "test, it"
   )
 }
